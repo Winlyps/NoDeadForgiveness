06d261a3e73e9a70906b2b6a6d0f8f08d3a5e30b
Winlypse <nts3@o2.pl>
2024-10-22 17:53:09 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noDeadForgiveness/NoDeadForgiveness.kt
@@ -0,0 +1,14 @@
+package winlyps.noDeadForgiveness
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoDeadForgiveness : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
