1e41fbf6734d34bada7d052812524fc511679ce9
Winlypse <nts3@o2.pl>
2024-10-22 17:53:19 +0200

    Changes

--- src/main/kotlin/winlyps/noDeadForgiveness/NoDeadForgiveness.kt
+++ src/main/kotlin/winlyps/noDeadForgiveness/NoDeadForgiveness.kt
@@ -6,9 +6,14 @@ class NoDeadForgiveness : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("forgiveDeadPlayers", "false")
+        }
+        logger.info("NoDeadForgiveness plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoDeadForgiveness plugin has been disabled.")
     }
 }
